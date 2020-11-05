796912236fe625a6c2ae357ab86b5b3dbdece953
Spikeone <fvv1@gmx.de>
2020-11-05 15:31:44 +0100

    Updated AprHint to Dordons current version

--- ArpHint/ArpHint.lua
+++ ArpHint/ArpHint.lua
@@ -1,11 +1,17 @@
 function ArpHint_OnLoad()
     this:RegisterEvent('PLAYER_TARGET_CHANGED');
-	this:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED');
+    this:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED');
 
     SLASH_ARPHINT1 = "/arphint";
     SlashCmdList["ARPHINT"] = function(msg)
         ArpHint_SlashCommandHandler(msg);
     end
+end
+
+-- always 2 decimals
+local function ArpHint_round(num)
+  local mult = 10^2
+  return math.floor(num * mult + 0.5) / mult
 end
 
 function ArpHint_SlashCommandHandler(msg)
@@ -18,17 +24,36 @@ function ArpHint_SlashCommandHandler(msg)
     end
 
     if (command == "show") then
-		getglobal("ArpHint_Frame"):Show()
-	elseif (command == "hide") then
-		getglobal("ArpHint_Frame"):Hide()
+        getglobal("ArpHint_Frame"):Show()
+        getglobal("ArpHint_FrameMH"):Show()
+        getglobal("ArpHint_FrameOH"):Show()
+    elseif (command == "hide") then
+        getglobal("ArpHint_Frame"):Hide()
+        getglobal("ArpHint_FrameMH"):Hide()
+        getglobal("ArpHint_FrameOH"):Hide()
     end
 
 end
 
 function ArpHint_OnEvent(self, event, ...)
-	local _, armor = UnitArmor("target")
-	
-	local armorPen = GetArmorPenetration()
-	
-	getglobal("ArpHint_ValueLabel"):SetText("" ..(armor - armorPen))
+    local _, armor = UnitArmor("target")
+    local mainSpeed, offSpeed = UnitAttackSpeed("player");
+    
+    if(mainSpeed == nil) then 
+        mainSpeed = 0
+    else
+        mainSpeed = ArpHint_round(mainSpeed)
+    end
+    
+    if(offSpeed == nil) then 
+        offSpeed = 0
+    else
+        offSpeed = ArpHint_round(offSpeed)
+    end
+
+    local armorPen = GetArmorPenetration()
+
+    getglobal("ArpHint_ValueLabel"):SetText("" ..(armor - armorPen))
+    getglobal("ArpHint_ValueMHLabel"):SetText("" ..mainSpeed)
+    getglobal("ArpHint_ValueOHLabel"):SetText("" ..offSpeed)
 end
